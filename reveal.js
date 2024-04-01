1427bebba224e391200ec535ceb76d11445df95e
HerickHannier <157404893+HerickHannier@users.noreply.github.com>
2024-04-01 11:13:23 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,78 @@
+window.revelar = ScrollReveal({reset:true})
+
+    revelar.reveal('.efeito-logo',
+    {
+        duration: 2000, 
+        distance:'90px'
+    })
+
+    revelar.reveal('.efeito-igm-top',
+    {
+        duration: 2000, 
+        distance:'90px'
+    })
+
+    revelar.reveal('.efeito-text-top',
+    {
+        duration: 2000, 
+        distance:'90px'
+    })
+    revelar.reveal('.efeito-botao-top',
+    {
+        duration: 2000, 
+        distance:'90px'
+    })
+    // Segunda pagina
+
+    revelar.reveal('.efeito-text-segunda',
+    {
+        duration: 2000, 
+        distance:'90px'
+    })
+
+    revelar.reveal('.efeito-img-segunda',
+    {
+        duration: 2000, 
+        distance:'90px'
+    })
+// Terceira pagina
+
+revelar.reveal('.efeito-text-terceira',
+{
+    duration: 2000, 
+    distance:'90px'
+})
+//Quarta pagina
+
+revelar.reveal('.efeito-img-quarta',
+{
+    duration: 2000, 
+    distance:'90px'
+})
+
+revelar.reveal('.efeito-text-quarta',
+{
+    duration: 2000, 
+    distance:'90px'
+})
+//Quinta pagina
+
+revelar.reveal('.efeito-quadrado',
+{
+    duration: 2000, 
+    distance:'90px'
+})
+
+revelar.reveal('.efeito-text-quinta',
+{
+    duration: 2000, 
+    distance:'90px'
+})
+//Sexta pagina
+
+revelar.reveal('.efeito-cadastro',
+{
+    duration: 2000, 
+    distance:'90px'
+})
+
